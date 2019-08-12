8f352cdaefdc681b16b8b0ff9ec1f0a25ddc3aa5
thevaderdude <valen@potloff.com>
2019-08-12 00:30:02 -0700

    big stank

--- app.js
+++ app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+app.use(express.static('public'));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+mongoose.connect('mongodb+srv://bobbyd:Amonalbus1!@cluster0-saber.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('connected to db!?!');
+}).catch(err => {
+	console.log('error:', err.message);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/', (req, res) => {
+	res.render("home.ejs");
 });
 
-module.exports = app;
+app.get('/demos', (req, res) => {
+	res.render("demos.ejs");
+});
+
+app.get('/documentation', (req, res) => {
+	res.render("documentation.ejs");
+});
+
+
+app.get('/testing', (req, res) => {
+	res.render("testing.ejs");
+});
+
+app.get('/color', (req, res) => {
+	res.render("color.ejs");
+});
+
+app.get('*', (req, res) => {
+	res.render("home.ejs");
+});
+
+app.listen(3000, () => {
+	console.log('server listening ');
+});
