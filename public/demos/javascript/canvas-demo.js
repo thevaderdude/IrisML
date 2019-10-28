7309f6d4ecd90d21f33b8a79f06b55faa8c2cdb1
BobDunkeroo <jamonalbus@gmail.com>
2019-10-27 22:50:56 -0700

    Fixed canvas demo

--- public/demos/javascript/canvas-demo.js
+++ public/demos/javascript/canvas-demo.js
@@ -1,22 +1,30 @@
 var clear = document.getElementById('clear');
+var guess = document.getElementById('guess');
+var canvasBig = document.getElementById('canvasBig');
+var ctxBig = canvasBig.getContext('2d');
+var canvasSmall= document.getElementById('canvasSmall');
+var ctxSmall = canvasBig.getContext('2d');
 
-var canvas = this.__canvas = new fabric.Canvas('canvas', {
+window.onload = function(){
+  $(".hide").remove();
+}
+
+var canvas = new fabric.Canvas('canvasBig', {
   isDrawingMode: true,
 });
-
-canvas.freeDrawingBrush.width = 50;
+canvas.freeDrawingBrush.width = 25;
 
 clear.addEventListener('click', function(){
   canvas.clear();
 });
 
-var imageSaver = document.getElementById('guess');
-
-imageSaver.addEventListener('click', function(e){
-  var canvas = document.getElementById("canvas");
-  var image = canvas.toDataURL("image/png", {
-    height: 28,
-    width: 28,
-  }); 
-}, false);
-
+guess.addEventListener('click', function(){
+  ctxSmall.drawImage(canvasBig, 0, 0, 280, 280, 0, 0, 28, 28);
+  var image = ctxSmall.getImageData(0,0, canvasSmall.width, canvasSmall.height).data;
+  var data = [];
+  for(var i = 0; i < image.length; i++){
+    if((i + 1) % 4 == 0){
+      data.push(image[i]);
+    }
+  }
+});
--- routes/netTrain.js
+++ routes/netTrain.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 
-// neural net
 router.post('/', function(req, res){
     checkData(req.body.net)
 });
