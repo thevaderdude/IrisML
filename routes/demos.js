129781e14eb5651db96c8d824284b33559125fbb
Yasper De Jong <yasperdejong@gmail.com>
2019-10-28 21:20:48 -0700

    Added instanceID generation

--- routes/demos.js
+++ routes/demos.js
@@ -11,7 +11,6 @@ router.post('/net', (req, res) => {
 	var item = {
 		TableName: "IrisMLDemos",
 		Item: {
-			"instanceID": 0,
 			"type": "net",
 			"activation-name": req.body.inputs[0],
 			"alpha": req.body.inputs[1],
@@ -26,21 +25,24 @@ router.post('/net', (req, res) => {
 		}
 	};
 	
-	docClient.put(item, function(err, data) {
-		if (err) {
-			console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-		} else {
-			console.log("Added item:", JSON.stringify(data, null, 2));
-		}
+	generateInstanceID(function(err, data) {
+		item.Item.instanceID = data;
+		docClient.put(item, function(err, data) {
+			if (err) {
+				console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+			} else {
+				console.log("Added item:", JSON.stringify(data, null, 2));
+			}
+		});
 	});
 });
 
 router.post('/slin', (req, res) => {
+
 	var docClient = new AWS.DynamoDB.DocumentClient();
 	var item = {
 		TableName: "IrisMLDemos",
 		Item: {
-			"instanceID": 0,
 			"type": "slin",
 			"alpha": req.body.inputs[0],
 			"lambda": req.body.inputs[1],
@@ -50,12 +52,15 @@ router.post('/slin', (req, res) => {
 		}
 	};
 	
-	docClient.put(item, function(err, data) {
-		if (err) {
-			console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-		} else {
-			console.log("Added item:", JSON.stringify(data, null, 2));
-		}
+	generateInstanceID(function(err, data) {
+		item.Item.instanceID = data;
+		docClient.put(item, function(err, data) {
+			if (err) {
+				console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+			} else {
+				console.log("Added item:", JSON.stringify(data, null, 2));
+			}
+		});
 	});
 });
 
@@ -64,7 +69,6 @@ router.post('/mlin', (req, res) => {
 	var item = {
 		TableName: "IrisMLDemos",
 		Item: {
-			"instanceID": 0,
 			"type": "mlin",
 			"alpha": req.body.inputs[0],
 			"lambda": req.body.inputs[1],
@@ -74,12 +78,15 @@ router.post('/mlin', (req, res) => {
 		}
 	};
 	
-	docClient.put(item, function(err, data) {
-		if (err) {
-			console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-		} else {
-			console.log("Added item:", JSON.stringify(data, null, 2));
-		}
+	generateInstanceID(function(err, data) {
+		item.Item.instanceID = data;
+		docClient.put(item, function(err, data) {
+			if (err) {
+				console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+			} else {
+				console.log("Added item:", JSON.stringify(data, null, 2));
+			}
+		});
 	});
 });
 
@@ -88,7 +95,6 @@ router.post('/bin', (req, res) => {
 	var item = {
 		TableName: "IrisMLDemos",
 		Item: {
-			"instanceID": 0,
 			"type": "bin",
 			"activation-name": req.body.inputs[0],
 			"alpha": req.body.inputs[1],
@@ -99,13 +105,29 @@ router.post('/bin', (req, res) => {
 		}
 	};
 	
-	docClient.put(item, function(err, data) {
-		if (err) {
-			console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-		} else {
-			console.log("Added item:", JSON.stringify(data, null, 2));
-		}
+	generateInstanceID(function(err, data) {
+		item.Item.instanceID = data;
+		docClient.put(item, function(err, data) {
+			if (err) {
+				console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+			} else {
+				console.log("Added item:", JSON.stringify(data, null, 2));
+			}
+		});
 	});
 });
+
+function generateInstanceID(callback) {
+	var dynamodb = new AWS.DynamoDB();
+	var id;
+	dynamodb.scan({ TableName: "IrisMLDemos" }, function(err, data) {
+		if (err) {
+			callback(err, null);
+		} else {
+			console.log("Itemcount: " + data.Count);
+			callback(null, data.Count);
+		}
+	});
+}
 
 module.exports = router;
